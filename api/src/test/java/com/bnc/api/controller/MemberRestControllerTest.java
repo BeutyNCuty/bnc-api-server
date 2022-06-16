54237963da15f4c6c8355781624bcf63bf94f683
yeji jeong <jy98102@naver.com>
2022-06-16 23:23:11 +0900

    BEUT-45 member api 개발

--- api/src/test/java/com/bnc/api/controller/MemberRestControllerTest.java
+++ api/src/test/java/com/bnc/api/controller/MemberRestControllerTest.java
@@ -1,19 +1,25 @@
 package com.bnc.api.controller;
 
-import com.bnc.api.controller.dto.MemberCreateDto;
 import com.bnc.api.controller.dto.MemberCreateDto.MemberCreatRequest;
+import com.bnc.api.controller.dto.MemberUpdateDto.MemberUpdateRequest;
 import com.bnc.api.support.BaseApiTest;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.bnc.common.member.domain.Member;
+import com.bnc.common.member.domain.MemberRepository;;
 import lombok.val;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static com.bnc.common.member.domain.MemberStatus.DELETED;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class MemberRestControllerTest extends BaseApiTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     void 회원가입_성공() throws Exception {
@@ -26,5 +32,52 @@ class MemberRestControllerTest extends BaseApiTest {
                         status().isOk(),
                         jsonPath("$.member.userId").value("test")
                 );
+    }
+
+    @Test
+    void 회원_조회_성공() throws Exception{
+        val mem = memberRepository.save(new Member("test", "1","어딘가","01012345678"));
+
+        mockMvc.perform(get("/detailMember/{id}", mem.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("id",mem.getId().toString()))
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.member.userId").value("test"),
+                        jsonPath("$.member.password").value("1"),
+                        jsonPath("$.member.addr").value("어딘가"),
+                        jsonPath("$.member.phone").value("01012345678")
+                );
+    }
+
+    @Test
+    void 회원_수정_성공() throws Exception{
+        val member = memberRepository.save(new Member("바보", "1","어딘가","01012345678"));
+
+        MemberUpdateRequest req = new MemberUpdateRequest("바보", "123", "화성", "123");
+
+        mockMvc.perform(patch("/modifyMember").
+                contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req)))
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.member.userId").value("바보"),
+                        jsonPath("$.member.password").value("123"),
+                        jsonPath("$.member.addr").value("화성"),
+                        jsonPath("$.member.phone").value("123")
+                );
+    }
+
+    @Test
+    void 회원_삭제_성공() throws Exception{
+        val member = memberRepository.save(new Member("바보", "1","어딘가","01012345678"));
+
+        mockMvc.perform(patch("/deleteMember/{id}", member.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("id",member.getId().toString()))
+                        .andExpectAll(
+                            status().isOk()
+                        );
+        assertThat(member.getMemberStatus()).isEqualTo(DELETED);
     }
 }
