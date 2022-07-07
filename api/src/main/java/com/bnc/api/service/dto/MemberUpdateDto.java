3057155ab71074eef5c16410c0ea7d6f5940a273
mumura <js_0811@naver.com>
2022-07-07 22:15:46 +0900

    BEUT-48 상품 등록 api 개발

---
+++ api/src/main/java/com/bnc/api/service/dto/MemberUpdateDto.java
@@ -0,0 +1,13 @@
+package com.bnc.api.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberUpdateDto {
+    private String userId;
+    private String password;
+    private String addr;
+    private String phone;
+}
